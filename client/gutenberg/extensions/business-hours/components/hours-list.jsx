551e470d56626dd09bdfda70865028c895e7770e
Rocco Tripaldi <tripaldir@gmail.com>
2019-02-07 16:15:59 -0500

    a fix for when the startOfTheWeek is not the default

--- client/gutenberg/extensions/business-hours/components/hours-list.jsx
+++ client/gutenberg/extensions/business-hours/components/hours-list.jsx
@@ -11,6 +11,7 @@ import { Component } from '@wordpress/element';
 
 import HoursRow from './hours-row';
 import apiFetch from '@wordpress/api-fetch/build/index';
+import { __ } from 'gutenberg/extensions/presets/jetpack/utils/i18n';
 
 const defaultLocalization = {
 	days: {
@@ -28,6 +29,7 @@ const defaultLocalization = {
 class HoursList extends Component {
 	state = {
 		localization: defaultLocalization,
+		hasFetched: false,
 	};
 
 	componentDidMount() {
@@ -38,10 +40,10 @@ class HoursList extends Component {
 		this.setState( { data: defaultLocalization }, () => {
 			apiFetch( { path: '/wpcom/v2/business-hours/localized-week' } ).then(
 				data => {
-					this.setState( { localization: data } );
+					this.setState( { localization: data, hasFetched: true } );
 				},
 				() => {
-					this.setState( { localization: defaultLocalization } );
+					this.setState( { localization: defaultLocalization, hasFetched: true } );
 				}
 			);
 		} );
@@ -50,16 +52,20 @@ class HoursList extends Component {
 	render() {
 		const { className, attributes } = this.props;
 		const { hours } = attributes;
-		const { localization } = this.state;
+		const { localization, hasFetched } = this.state;
 		const { startOfWeek } = localization;
 		return (
 			<dl className={ className }>
-				{ Object.keys( hours )
-					.concat( Object.keys( hours ).slice( 0, startOfWeek ) )
-					.slice( startOfWeek )
-					.map( dayOfTheWeek => {
-						return <HoursRow day={ dayOfTheWeek } data={ localization } { ...this.props } />;
-					} ) }
+				{ hasFetched ? (
+					Object.keys( hours )
+						.concat( Object.keys( hours ).slice( 0, startOfWeek ) )
+						.slice( startOfWeek )
+						.map( dayOfTheWeek => {
+							return <HoursRow day={ dayOfTheWeek } data={ localization } { ...this.props } />;
+						} )
+				) : (
+					<p>{ __( 'Loading...' ) }</p>
+				) }
 			</dl>
 		);
 	}
