9ecce45647ac17f077f2ecf4cf44faecd77e22b7
Jon Surrell <jon.surrell@automattic.com>
2019-02-12 11:09:20 +0100

    Avoid premature requests when dimensions are unknown

--- client/gutenberg/extensions/tiled-gallery/layout/index.js
+++ client/gutenberg/extensions/tiled-gallery/layout/index.js
@@ -147,7 +147,7 @@ function photonWpcomImage( url, opts = {} ) {
 function buildSrcset( img, { layoutStyle } ) {
 	const { url, width } = img;
 
-	if ( ! url ) {
+	if ( ! img.height || ! url || ! width ) {
 		return;
 	}
 
@@ -191,7 +191,7 @@ const defaultPhotonArgs = {
  * @return {string} Photon URL
  */
 function photonize( { height, width, url }, photonArgs = {}, { layoutStyle } = {} ) {
-	if ( ! url ) {
+	if ( ! height || ! url || ! width ) {
 		return;
 	}
 
